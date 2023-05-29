94a6e7b5e9047ebe2bc6b9f682f5a546bb9e9d02
Sidsloth-k <122206830+Sidsloth-k@users.noreply.github.com>
2023-05-30 01:43:00 +0300

    Add files via upload

---
+++ slats html/assests/js/buttons click.js
@@ -0,0 +1,33 @@
+
+
+// Add click event listeners to the buttons
+const withdrawButton = document.querySelector('nav a button');
+const withdrawButtonHandler = () => 
+{
+     href=""
+};
+withdrawButton.addEventListener('click', withdrawButtonHandler);
+
+const sendButton = document.querySelectorAll('.buttons-container button')[0];
+const sendButtonHandler = () => {
+	// Handle send button click here
+};
+sendButton.addEventListener('click', sendButtonHandler);
+
+const withdrawButton2 = document.querySelectorAll('.buttons-container button')[1];
+const withdrawButton2Handler = () => {
+	// Handle withdraw button 2 click here
+};
+withdrawButton2.addEventListener('click', withdrawButton2Handler);
+
+const depositButton = document.querySelectorAll('.buttons-container button')[2];
+const depositButtonHandler = () => {
+	// Handle deposit button click here
+};
+depositButton.addEventListener('click', depositButtonHandler);
+
+const linkButton = document.querySelector('.link-container button');
+const linkButtonHandler = () => {
+	// Handle link button click here
+};
+linkButton.addEventListener('click', linkButtonHandler);
